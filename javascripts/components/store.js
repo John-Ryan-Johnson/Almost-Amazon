3cb3dbabe9618a5d2b9cd0dd7437027bdea9a6d3
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-09-23 19:51:55 -0500

    added store.js and components and assets

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,12 +1,31 @@
+const bookInfo = {
+  title: 'My Book',
+  price: '19.99',
+  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6Ia_urbUqW8fArIKsRqUQ9B8sVPZBbO0ISQuavN5ZgpGAIWaPGQ'
+}
+
+
+
+
 const printToDom = (divId, toPrint) => {
   const selectedDiv = document.getElementById(divId);
   selectedDiv.innerHTML = toPrint;
 };
 
+const addToCartEvent = () => {
+  console.log('added to cart');
+}
+
 
 const makeStore = () => {
-  const domString = "hi  new store";
-  printToDom("store-container", domString)
+  let domString = '<h2>Our only book:</h2>';
+  domString += '<p>Buy it now:</p>';
+  domString += `<h3>$${bookInfo.price}</h3>`;
+  domString += `<img src=${bookInfo.image} alt="book cover"/>`;
+  domString += '<button id="cart-button" class="btn btn-danger col-12">Add to cart</button>';
+
+  printToDom('store-container', domString);
+  document.getElementById('cart-button').addEventListener('click', addToCartEvent);
 };
 
 
